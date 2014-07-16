d6f11571f16efea8772554f6db85cde3dc010c47
Gert Vanthienen <gert.vanthienen@gmail.com>
2014-07-16 23:51:01 +0200

    ENTESB-1707: Use Activator for registering fab: URL handler

--- fab/fab-osgi/src/main/java/io/fabric8/fab/osgi/internal/Activator.java
+++ fab/fab-osgi/src/main/java/io/fabric8/fab/osgi/internal/Activator.java
@@ -14,24 +14,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.fabric8.fab.osgi.internal;
 
 import java.io.File;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
 
+import io.fabric8.fab.osgi.FabResolverFactory;
+import io.fabric8.fab.osgi.FabURLHandler;
 import io.fabric8.fab.osgi.ServiceConstants;
-import org.ops4j.pax.url.commons.handler.HandlerActivator;
+import org.apache.karaf.features.FeaturesService;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.url.URLStreamHandlerService;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * Activator for the fab protocol
  */
-public class Activator extends HandlerActivator<Configuration> {
+public class Activator implements BundleActivator, ServiceTrackerCustomizer {
     private static Activator instance;
 
     private BundleContext bundleContext;
+
+    private List<ServiceRegistration> registrations = new LinkedList<ServiceRegistration>();
+
+    private FabResolverFactoryImpl factory;
+
     public static OsgiModuleRegistry registry = new OsgiModuleRegistry();
 
     public static Activator getInstance() {
@@ -47,12 +61,12 @@ public class Activator extends HandlerActivator<Configuration> {
     }
 
     public Activator() {
-        super(ServiceConstants.PROTOCOLS_SUPPORTED, ServiceConstants.PID, new FabConnectionFactory());
+        super();
         instance = this;
     }
 
     @Override
-    public void start(BundleContext bundleContext) {
+    public void start(final BundleContext bundleContext) {
         this.bundleContext = bundleContext;
 
         ServiceReference serviceReference = bundleContext.getServiceReference("org.osgi.service.cm.ConfigurationAdmin");
@@ -64,10 +78,82 @@ public class Activator extends HandlerActivator<Configuration> {
         registry.setPid("io.fabric8.fab.osgi.registry");
         registry.load();
 
-        super.start(bundleContext);
+        // Create and register the FabResolverFactory
+        factory = new FabResolverFactoryImpl();
+        factory.setBundleContext(bundleContext);
+        factory.setConfigurationAdmin(configurationAdmin);
+        registerFabResolverFactory(factory);
+
+        // track FeaturesService for use by factory
+        ServiceTracker featuresServiceTracker = new ServiceTracker(bundleContext, FeaturesService.class, this);
+        featuresServiceTracker.open();
+
+        // Create and register the fab: URL handler
+        FabURLHandler handler = new FabURLHandler();
+        handler.setFabResolverFactory(factory);
+        handler.setServiceProvider(factory);
+        registerURLHandler(handler);
+    }
+
+    /*
+     * Register the URL handler
+     */
+    private void registerURLHandler(FabURLHandler handler) {
+        if (bundleContext != null && handler != null) {
+            Hashtable props = new Hashtable();
+            props.put("url.handler.protocol", ServiceConstants.PROTOCOL_FAB);
+            ServiceRegistration registration = bundleContext.registerService(URLStreamHandlerService.class, handler, props);
+            registrations.add(registration);
+        }
+    }
+
+    /*
+     * Register the {@link FabResolverFactory}
+     */
+    private void registerFabResolverFactory(FabResolverFactoryImpl factory) {
+        if (bundleContext != null && factory != null) {
+            ServiceRegistration registration = bundleContext.registerService(FabResolverFactory.class, factory, null);
+            registrations.add(registration);
+        }
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        for (ServiceRegistration registration : registrations) {
+            if (registration != null) {
+                registration.unregister();
+            }
+        }
+        registrations.clear();
+        factory = null;
     }
 
     public BundleContext getBundleContext() {
         return bundleContext;
+    }
+
+    @Override
+    public Object addingService(ServiceReference serviceReference) {
+        Object service = bundleContext.getService(serviceReference);
+        if (service instanceof FeaturesService) {
+            factory.setFeaturesService((FeaturesService) service);
+        }
+        return service;
+    }
+
+    @Override
+    public void modifiedService(ServiceReference serviceReference, Object o) {
+        // properties have changed, no need to do anything here
+    }
+
+    @Override
+    public void removedService(ServiceReference serviceReference, Object o) {
+        if (o instanceof FeaturesService && factory != null) {
+            FeaturesService service = (FeaturesService) o;
+            if (service == factory.getFeaturesService()) {
+                factory.setFeaturesService(null);
+            }
+        }
+
     }
 }
