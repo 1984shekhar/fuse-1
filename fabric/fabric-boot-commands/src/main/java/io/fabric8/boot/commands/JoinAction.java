4c246773eaa31967e99444b52f0a4bf2eb87ea84
Hiram Chirino <hiram@hiramchirino.com>
2014-08-14 13:30:42 -0400

    Fixes ENTESB-1780 - fabric:join should not rename container's name

--- fabric/fabric-boot-commands/src/main/java/io/fabric8/boot/commands/JoinAction.java
+++ fabric/fabric-boot-commands/src/main/java/io/fabric8/boot/commands/JoinAction.java
@@ -100,6 +100,12 @@ final class JoinAction extends AbstractAction {
     @Override
     protected Object doExecute() throws Exception {
         String oldName = runtimeProperties.getProperty(SystemProperties.KARAF_NAME);
+
+        if( System.getenv("OPENSHIFT_BROKER_HOST")!=null &&  containerName!=null ) {
+            System.err.println("Containers in OpenShift cannot be renamed");
+            return null;
+        }
+
         if (containerName == null) {
             containerName = oldName;
         }
