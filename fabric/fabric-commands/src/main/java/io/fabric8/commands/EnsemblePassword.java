bed095a580b397031860f2f16c5e87737428a3c5
Hiram Chirino <hiram@hiramchirino.com>
2015-03-09 11:17:53 -0400

    Fixes PATCH-645: ported [FABRIC-479]Fuse Fabric should have option to change ensemble password easily.

--- fabric/fabric-commands/src/main/java/io/fabric8/commands/EnsemblePassword.java
+++ fabric/fabric-commands/src/main/java/io/fabric8/commands/EnsemblePassword.java
@@ -16,15 +16,95 @@
  */
 package io.fabric8.commands;
 
-import org.apache.felix.gogo.commands.Command;
+import io.fabric8.api.Profile;
+import io.fabric8.api.Version;
 import io.fabric8.boot.commands.support.FabricCommand;
+import io.fabric8.utils.PasswordEncoder;
+import io.fabric8.zookeeper.ZkPath;
+import io.fabric8.zookeeper.curator.CuratorACLManager;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
+import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
-@Command(name = "ensemble-password", scope = "fabric", description = "Display the ensemble password", detailedDescription = "classpath:ensemblePassword.txt")
+@Command(name = "ensemble-password", scope = "fabric", description = "Display or update the ensemble password", detailedDescription = "classpath:ensemblePassword.txt")
 public class EnsemblePassword extends FabricCommand {
+
+    @Argument(index = 0, name = "new-password", description = "The new ensemble password.  If not supplied, the password is displayed.")
+    private String newPassword;
+
+    @Option(name = "-c", aliases = {"--commit"}, multiValued = false, required = false, description = "Commits the password change.")
+    protected boolean commit = false;
 
     @Override
     protected Object doExecute() throws Exception {
-        System.out.println(fabricService.getZookeeperPassword());
+        if( !commit ) {
+
+            if( newPassword==null ) {
+                System.out.println(fabricService.getZookeeperPassword());
+            } else {
+                String zookeeperUrl = fabricService.getZookeeperUrl();
+                String oldPassword = fabricService.getZookeeperPassword();
+
+                System.out.println("Updating the password...");
+
+                // Since we will be changing the password, create a new ZKClient that won't
+                // be getting update by the password change.
+                CuratorACLManager aclManager = new CuratorACLManager();
+
+                CuratorFramework curator = CuratorFrameworkFactory.builder().connectString(zookeeperUrl).retryPolicy(new RetryOneTime(500))
+                        .aclProvider(aclManager).authorization("digest", ("fabric:" + oldPassword).getBytes()).sessionTimeoutMs(30000)
+                        .build();
+                curator.start();
+                try {
+
+                    // Lets first adjust the acls so that the new password and old passwords work against the ZK paths.
+                    String digestedIdPass = DigestAuthenticationProvider.generateDigest("fabric:" + newPassword);
+                    aclManager.registerAcl("/fabric", "auth::acdrw,world:anyone:,digest:" + digestedIdPass + ":acdrw");
+                    aclManager.fixAcl(curator, "/fabric", true);
+
+                    // Ok now lets push out a config update of what the password is.
+                    curator.setData().forPath(ZkPath.CONFIG_ENSEMBLE_PASSWORD.getPath(), PasswordEncoder.encode(newPassword).getBytes("UTF-8"));
+                } finally {
+                    curator.close();
+                }
+
+                // Refresh the default profile to cause all nodes to pickup the new password.
+                for (Version version : fabricService.getVersions()) {
+                    Profile profile = version.getProfile("default");
+                    if (profile != null) {
+                        profile.refresh();
+                    }
+                }
+
+                System.out.println("");
+                System.out.println("Password updated. Please wait a little while for the new password to");
+                System.out.println("get delivered as a config update to all the fabric nodes. Once, all the ");
+                System.out.println("nodes are updated (nodes must be online), please run:");
+                System.out.println("");
+                System.out.println("  fabric:ensemble-password --commit ");
+                System.out.println("");
+
+            }
+        } else {
+
+            // Now lets connect with the new password and reset the ACLs so that the old password
+            // does not work anymore.
+            CuratorACLManager aclManager = new CuratorACLManager();
+            CuratorFramework curator = CuratorFrameworkFactory.builder().connectString(fabricService.getZookeeperUrl()).retryPolicy(new RetryOneTime(500))
+                    .aclProvider(aclManager).authorization("digest", ("fabric:" + fabricService.getZookeeperPassword()).getBytes()).sessionTimeoutMs(30000)
+                    .build();
+            curator.start();
+            try {
+                aclManager.fixAcl(curator, "/fabric", true);
+                System.out.println("Only the current password is allowed access to fabric now.");
+            } finally {
+                curator.close();
+            }
+        }
         return null;
     }
 
